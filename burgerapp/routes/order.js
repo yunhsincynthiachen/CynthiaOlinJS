554f1af7c1a3abbbf20e9197645291de3f02b006
Sarah Walters <sarah.walters@students.olin.edu>
2016-02-22 23:36:41 -0500

    Homework 5 feedback

--- burgerapp/routes/order.js
+++ burgerapp/routes/order.js
@@ -5,6 +5,8 @@ var Burger = require('../models/appModel.js').BurgerModel;
 
 var mongoose = require('mongoose');
 var db = mongoose.connection;
+// shouldn't need mongoose or db here if you've connected to the database in app.js --
+// the Mongoose models are enough
 
 var async = require("async");
 
@@ -45,22 +47,22 @@ routes.getOrderIngredientsGET = function(req,res) {
       res.json({"error":"burger not found"});
       return;
     }
-    else {
+    // don't need the else -- you returned in the if
 		var burgers_ing = {}
 		var burgers = []
 		var tot_price = 0;
-		async.each([burger], function(burger, callback) {
+		async.each([burger], function(burger, callback) { // nice!
     		Ingredient.find({
 			    '_id': { $in: burger['ingredients']}
-			}, function(err, docs){
+		  }, function(err, docs){
 				docs.forEach(function(entry) {
 					tot_price = tot_price + entry['price'];
-				    burgers.push(entry['name']);
+				  burgers.push(entry['name']);
 				});
-				console.log(burgers)
+
 				burgers_ing['list_ing'] = burgers;
 				burgers_ing['totprice'] = tot_price;
-			    callback();
+			  callback();
 			});
 		}, function(err){
 		    // if any of the file processing produced an error, err would equal that error
@@ -73,9 +75,14 @@ routes.getOrderIngredientsGET = function(req,res) {
       			return;
 		    }
 		});
-    }
+    /* If you wanted to update price without making a database query (and without a second
+       ajax request in your success handler!) you could have sent the total price to the database
+       when placing the order, along with all of the ingredients in the order.
+       You're displaying prices on the place-an-order page next to the ingredient checkboxes, so you
+       have enough information to figure out the total price of the order on the clientside.
+    */
   });
-}
+};
 
 //Create the order and save the burger data to the server:
 routes.getOrderPOST = function(req, res) {
